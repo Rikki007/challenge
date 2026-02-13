a6e199d1d17019eea32fa97a0f9b91e99dd5161d
Kirill Oreshkevich <oreshaodnoglazy@gmail.com>
2026-02-13 11:25:59 +0300

    fix: work on ui

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import testCreator from './test_generator/testCreator.js';
 import testQuestions from './test_generator/testQuestions.js';
-import preloader from './preloader/preloader.js';
 import initApp from './initApp.js';
 
 initApp();
--- initApp.js
+++ initApp.js
@@ -1,13 +1,8 @@
 import testCreator from './test_generator/testCreator.js';
 import testQuestions from './test_generator/testQuestions.js';
-import preloader from './preloader/preloader.js';
 
 const initApp = async () => {
     const main = document.querySelector('.main');
-    
-    main.innerHTML = `
-        <div class="preloader-window">${preloader()}</div>
-    `;
 
     await new Promise(r => setTimeout(r));
 
